cade274508d23e5d1066479c91454a48dd140472
onlyvm10 <vatsalmishra271@gmail.com>
2024-05-15 16:18:19 +0530

    msg

---
+++ Assignment-1/Ques-2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Ques_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Argument1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Argument2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Operand: ");
+            string op = Console.ReadLine();
+
+
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine("Sum is " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine("Difference is " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine("Product is " + (num1 * num2));
+                    break;
+
+                case "/":
+                    if (num1 != 0)
+                    {
+                        Console.WriteLine("Quotient is " + (num1 / num2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't divide by zero");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choice Entered");
+                    break;
+            }
+            Console.WriteLine("Program Exited");
+        }
+    }
+}
---
+++ Assignment-1/Ques-3/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Ques_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+            int choice;
+            int num2;
+            string op;
+            do
+            {
+                Console.WriteLine("1.Addition");
+                Console.WriteLine("2.Subtraction");
+                Console.WriteLine("3.Multiplication");
+                Console.WriteLine("4.Division");
+                Console.WriteLine("0.Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine("Enter Arg1: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Arg2: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+              
+
+                switch (choice) {
+                    case 1:
+                        Console.WriteLine("Sum is "+(num1+num2));
+                        break;
+                    case 2:
+                        Console.WriteLine("Difference is " + (num1 - num2));
+                        break;
+                    case 3:
+                        Console.WriteLine("Product is " + (num1 * num2));
+                        break;
+                    case 4:
+                        if (num2 != 0)
+                        {
+                            Console.WriteLine("Quotient is " + (num1 / num2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Can not divide by zero");
+                        }
+
+                        break;
+                   
+                        
+                    default:
+                        Console.WriteLine("Wrong Option Entered");
+                        break;
+                }
+
+
+            } while (choice != 0);
+            Console.WriteLine("Thanks for using my Application!");
+        }
+    }
+}
